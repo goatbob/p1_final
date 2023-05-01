e48cfb1abb245f45063004d6d50cbf45411cc690
goatbob <122817160+goatbob@users.noreply.github.com>
2023-05-01 12:04:57 -0500

    Update PIT_subclass.py

--- incident_classes/PIT_subclass.py
+++ incident_classes/PIT_subclass.py
@@ -1,4 +1,31 @@
+"""
+program: PIT_subclass.py
+author: kyle godwin
+last date modified: 01 may 2023
+"""
+from incident_classes import Incident
 
 
-collision
-operator experience
+class PIT(Incident):
+    """NearMiss subclass of base class Incident"""
+    def __init__(self, date, time, employee, incident_type="PIT", **kwargs):
+        super().__init__(date, time, employee, incident_type)
+        self.PITs = []
+        try:
+            self.impact = kwargs["impact"]
+            self.experience = kwargs["experience"]
+        except KeyError:
+            raise IncorrectArgumentsError
+
+    def __str__(self):
+        return f"{self.inc_employee} involved in {self.inc_type} on {self.inc_date} at {self.inc_time}." \
+               f"\n{self.nearMiss_type} of {self.nearMiss_severity} severity." \
+               f"\nSubmitted: {self.submit_date}"
+
+    def __repr__(self):
+        return f"Injury('{self.inc_date}', '{self.inc_time}', '{self.inc_employee}', " \
+               f"inj_type='{self.nearMiss_type}', body_part='{self.nearMiss_severity}')"
+
+
+class IncorrectArgumentsError(Exception):
+    pass
